62dd8289509dbbbd05c039596b7da1d3ae1a3ca5
thecreatorpc101@gmail.com <72680645+thecreatorpc@users.noreply.github.com>
2022-12-27 14:33:10 +0530

    added notes

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* this app can be understood as the entire react application */}
   </React.StrictMode>
 );
 
@@ -15,3 +16,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// So what's happening here is that our index.js is looking for some element with an idea of root. And as we see in the index.html , there is a div which has the ID of root. So what React is saying is, oh, I want you to take that div and I want you to replace that div and render into it our entire React application(<App/> this is present in the <React.StrictMode> in the index.js itself).And that's what this is doing right here.So that's pretty much how React actually works with HTML.
+// For making react applications we need to import react and react dom , react dom is for the web.
